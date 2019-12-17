ec37a868361b7e7a05348741ca1240dc8d13b234
Max <hello@max.berlin>
2019-12-17 18:35:46 +0100

    Add: loading screen with welcome text

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,19 +5,44 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="/assets/css/main.css">
-        <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,800&display=swap" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,800&display=swap" rel="stylesheet">
         <link rel="shortcut icon" type="image/svg" href="/assets/images/logo.png" />
         <!-- Primary Meta Tags -->
         <title>Humba - Mache mehr aus deiner Pause</title>
         <meta name="title" content="Humba - Mache mehr aus deiner Pause">
         <meta name="description" content="Humba ist die App mit der du deine Freistunde oder Pause endlich sinnvoller verbringen kannst. Suche einfach Andere, die auch frei haben, lerne neue Menschen kennen, lass dir von uns Aktivitäten vorschlagen oder gehe mit günstiger bei unseren Partnern essen.">
         <style media="screen">
-            /* body {
+            body {
                 font-family: 'Montserrat', sans-serif;
-            } */
+            }
+            #page{
+              display: none;
+            }
+            #loading{
+              display: block;
+            }
+            .animation {
+              animation-name: spin;
+              animation-duration: 4000ms;
+              animation-iteration-count: infinite;
+              animation-timing-function: linear
+            }
+            @keyframes spin {
+                from {transform:rotate(0deg);}
+                to {transform:rotate(360deg);}
+            }
         </style>
+        <script>
+            function myFunction()
+            {
+              setTimeout(function(){
+                document.getElementById("page").style.display = "block";
+                document.getElementById("loading").style.display = "none";
+              }, 2000);
+            }
+        </script>
     </head>
-    <body class="bg-humba-black">
+    <body  onload="myFunction()" class="bg-humba-black">
         @yield('body')
     </body>
     <script>
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,9 +1,18 @@
 @extends('_layouts.master')
 
 @section('body')
+<div id="page">
 <div class="flex h-screen">
   <div class="m-auto">
-    <img src="/assets/images/logo.png" class="h-16 w-16" alt="">
+    <p class="text-humba-pink font-semibold">welcome!</p>
+  </div>
+</div>
+</div>
+<div id="loading">
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <img src="/assets/images/logo.png" class="animation my-auto h-16 w-16" alt="">
+    </div>
   </div>
 </div>
 @endsection
