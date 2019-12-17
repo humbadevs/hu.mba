be786b27f04be718584dbe9a3727b7d9fb301a01
Max <hello@max.berlin>
2019-12-17 17:37:26 +0100

    Add: tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'humba-pink': '#FA7268',
+        'humba-black': '#151515'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
