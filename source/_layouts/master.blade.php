0e2863315e8b5d8099e295e91ea7f767e2c8b454
Max <hello@max.berlin>
2019-12-17 17:37:42 +0100

    Add: master layout

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,9 +4,33 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet" href="/assets/css/main.css">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,800&display=swap" rel="stylesheet"> -->
+        <link rel="shortcut icon" type="image/svg" href="/assets/images/logo.png" />
+        <!-- Primary Meta Tags -->
+        <title>Humba - Mache mehr aus deiner Pause</title>
+        <meta name="title" content="Humba - Mache mehr aus deiner Pause">
+        <meta name="description" content="Humba ist die App mit der du deine Freistunde oder Pause endlich sinnvoller verbringen kannst. Suche einfach Andere, die auch frei haben, lerne neue Menschen kennen, lass dir von uns Aktivitäten vorschlagen oder gehe mit günstiger bei unseren Partnern essen.">
+        <style media="screen">
+            /* body {
+                font-family: 'Montserrat', sans-serif;
+            } */
+        </style>
     </head>
-    <body>
+    <body class="bg-humba-black">
         @yield('body')
     </body>
+    <script>
+        (function(f, a, t, h, o, m){
+        	a[h]=a[h]||function(){
+        		(a[h].q=a[h].q||[]).push(arguments)
+        	};
+        	o=f.createElement('script'),
+        	m=f.getElementsByTagName('script')[0];
+        	o.async=1; o.src=t; o.id='fathom-script';
+        	m.parentNode.insertBefore(o,m)
+        })(document, window, '//analytics.max.berlin/tracker.js', 'fathom');
+        fathom('set', 'siteId', 'GVGUD');
+        fathom('trackPageview');
+    </script>
 </html>
